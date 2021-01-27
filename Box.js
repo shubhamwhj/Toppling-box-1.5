de6ef68cc33ec78d8c552974901835f994dd8e0e
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2021-01-27 13:38:06 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,22 @@
+class Box
+{
+    constructor(x,y,width,height)
+    {
+        var option={
+            'restitution' : 0.8
+        }
+        this.body=Bodies.rectangle(x,y,width,height,option);
+        World.add(world,this.body);
+        
+        this.width=width;
+        this.height=height;
+    }
+
+    display()
+    {
+        var pos=this.body.position;
+        rectMode(CENTER);
+        rect(pos.x,pos.y,this.width,this.height);    
+    }
+}
+
